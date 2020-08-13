f57455c513337132134bf4244df7c727209f0c88
Darbaz Ali <darbaz.a.h@gmail.com>
2020-08-13 15:09:45 +0300

    Solve first Challenge

---
+++ Day_0/helloWorld.js
@@ -0,0 +1,16 @@
+
+// create a fuction that takes one argument, it should be astring
+// pring Hello world in the first line
+// print what ever the string of the argument in the second line
+
+const processData = str => {
+    console.log("Hello, World.");
+    console.log(str);
+}
+
+processData('Welcome to 30 Days of Code!')
+/* 
+    prints:
+    Hello, World.
+    Welcome to 30 Days of Code!
+*/
